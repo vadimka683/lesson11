e8d14abf18de1985b1b7878bb2ce1d3967c117a4
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-09 16:44:43 +0300

    переделанная логика шифра цезаря

--- Задача1/Задача1/main1.cpp
+++ Задача1/Задача1/main1.cpp
@@ -6,13 +6,15 @@ string encrypt_caesar(string text, int code) {
 	string Code_Text = "";
 	for (int i = 0; i < text.length(); i++) {
 		if (text[i] >= 'A' && text[i] <= 'Z') {
-			int Code_char = (int)text[i];
-			Code_char = ((Code_char + code) <= (int)'Z') ? Code_char + code : code - ((int)'Z' - Code_char) + (int)'A';
+			int Code_char = (int)text[i] - (int)'A';
+			Code_char = (Code_char + code) % 26;
+			Code_char += (int)'A';
 			Code_Text += (char)Code_char;
 		}
 		else if (text[i] >= 'a' && text[i] <= 'z') {
-			int Code_char = (int)text[i];
-			Code_char = ((Code_char + code) <= (int)'z') ? Code_char + code : code - ((int)'z' - Code_char) + (int)'a';
+			int Code_char = (int)text[i] - (int)'a';
+			Code_char = (Code_char + code) % 26;
+			Code_char += (int)'a';
 			Code_Text += (char)Code_char;
 		}
 		else {
